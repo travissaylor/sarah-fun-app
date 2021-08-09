300fd02a77b946431b082d47a8313303cfa3b1b8
Travis Saylor <tsaylor@Traviss-MBP.attlocal.net>
2021-08-09 16:53:29 -0500

    Add avatar to the nav

--- nav.js
+++ nav.js
@@ -1,5 +1,5 @@
 import { FiHome } from "react-icons/fi"
-import { FaCat } from "react-icons/fa"
+import { FaCat, FaYinYang } from "react-icons/fa"
 import { CgNotes } from "react-icons/cg"
 import { ImOffice, ImBooks } from "react-icons/im"
 
@@ -8,5 +8,6 @@ export const LinkItems = [
     { name: "Notes", route: "/notes", icon: CgNotes },
     { name: "The Office", route: "/office", icon: ImOffice },
     { name: "Marvel", route: "/marvel", icon: ImBooks },
+    { name: "Avatar", route: "/avatar", icon: FaYinYang },
     { name: "Cats", route: "/cats", icon: FaCat },
 ]
